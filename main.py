5e0ec45b6c0650a6f18d553e23d55a10d87915df
ferrevdd <ferre.vandendriessche@gmail.com>
2022-05-01 12:59:00 +0200

    made it better

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
-#---start of importing modules---
-import os                      #-
-from time import sleep         #-
-import ctypes                  #-
-import subprocess              #-
-import winsound                #-
-#----end of importing modules----
-
-
-#----------start of settings-------------              -------------------------------------------            
-staticprocname = 'RobloxPlayerBeta.exe'#-              - robloxprocessname (leave it)            -                 
-HiddenMode = 0                         #-              -console visible or not (0 or 1)          -
-NotifSound = 0                         #-              -sound when the process is found (0 or 1) -             
-#----------end of settings---------------              -------------------------------------------
-
-if HiddenMode == 1:
-    ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 0 )
-else:
-    pass
-
-def findroblox(process_name):
-    call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
-
-    output = subprocess.check_output(call).decode()
+def maincfunction():
     
-    last_line = output.strip().split('\r\n')[-1]
-    
-    return last_line.lower().startswith(process_name.lower())
-StarterAlreadyStarted = 0
 
-while True:
+    #---start of importing modules---
+    import os                      #-
+    from time import sleep         #-
+    import ctypes                  #-
+    import subprocess              #-
+    import winsound                #-
+    #----end of importing modules----
+
+
+    #----------start of settings-------------              -------------------------------------------            
+    staticprocname = 'RobloxPlayerBeta.exe'#-              - robloxprocessname (leave it)            -                 
+    HiddenMode = 0                         #-              -console visible or not (0 or 1)          -
+    NotifSound = 0                         #-              -sound when the process is found (0 or 1) -             
+    #----------end of settings---------------              -------------------------------------------
+
+    if HiddenMode == 1:
+        ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 0 )
+    else:
+        pass
+
+    def findroblox(process_name):
+        call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
+
+        output = subprocess.check_output(call).decode()
     
-    if findroblox(staticprocname) == True and StarterAlreadyStarted == 0:
-        print('Roblox Process has been found!')
+        last_line = output.strip().split('\r\n')[-1]
+    
+        return last_line.lower().startswith(process_name.lower())
+    
+    StarterAlreadyStarted = 0
+
+    while True:
+    
+        if findroblox(staticprocname) == True and StarterAlreadyStarted == 0:
+            print('Roblox Process has been found!')
         
-        if NotifSound == 1:
-            winsound.Beep(440, 250)
+            if NotifSound == 1:
+                winsound.Beep(440, 250)
             
-        Files = os.listdir(os.getcwd()) 
+            Files = os.listdir(os.getcwd()) 
         
-        for file in Files:
+            for file in Files:
             
-            if file.startswith("rbx"): 
+                if file.startswith("rbx"): 
                 
-                GrabbedExec = file
+                    GrabbedExec = file
                 
-                print('found the fpsunlocker executable!')
+                    print('found the fpsunlocker executable!')
         
         
-        subprocess.Popen(os.getcwd()+'/'+GrabbedExec) 
+            subprocess.Popen(os.getcwd()+'/'+GrabbedExec) 
         
-        StarterAlreadyStarted = 1
+            StarterAlreadyStarted = 1
         
-        print('finished the starter mechanism!')
+            print('finished the starter mechanism!')
     
-    elif findroblox(staticprocname) == False and StarterAlreadyStarted == 1:
+        elif findroblox(staticprocname) == False and StarterAlreadyStarted == 1:
         
-        os.system('taskkill /im '+GrabbedExec+ ' /f')
+            os.system('taskkill /im '+GrabbedExec+ ' /f')
+
+            StarterAlreadyStarted = 0
         
-        StarterAlreadyStarted = 0
+            print('sucessfully killed the fpsunlocker process!')
         
-        print('sucessfully killed the fpsunlocker process!')
+            sleep(3)
         
-        sleep(3)
+            os.system('cls')
         
-        os.system('cls')
+        sleep(1)
+        
     
     
+    
+
+if __name__ == "__main__":
+    maincfunction()
