ba6d504ff7d0a86280ef01cf9d7d0fb771d21644
ferrevdd <ferre.vandendriessche@gmail.com>
2022-05-01 09:59:00 +0200

    fixed taskschedule

--- RunMeToAddScheduledTask.py
+++ RunMeToAddScheduledTask.py
@@ -8,7 +8,12 @@ def is_admin():
 
 if is_admin():
     import os
-    os.system(r'SchTasks /Create /SC ONLOGON /TN "auto-fps-unlock" /TR "'+os.getcwd()+'/main.py"')
-else:
+    import time
+    print('Please type the password of the current user : \n\n\n')
+    os.system('SchTasks /Create /SC ONLOGON /V1 /TN "auto-fps-unlock" /TR \"'+os.getcwd()+r'\main.py'+ '"')
+    print('task has been created sucessfully , script will automatically launch on next logon!')
+    print('\n\nquitting in 5 sec...')
+    time.sleep(5)
+else:                                                                       
     
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
